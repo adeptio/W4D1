d439a0ac02818d7e39d6a325170934e48cc1ffab
Mike Brinkman <mbrinkma13@gmail.com>
2017-08-21 23:36:35 -0700

    finished phase 2

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
   def index
-    users = User.all
-    render json: users
+    @users = User.all
+    render json: @users
   end
 
   def create
-    user = User.new(params[:user].permit(:name, :email))
-    if user.save
-      render json: user
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
     else
-      render json: user.errors.full_messages, status: :unprocessable_entity
+      render json: @user.errors.full_messages, status: :unprocessable_entity
     end
   end
 
@@ -18,23 +18,23 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    if user.update_attributes(user_params)
-      render json: user
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      render json: @user
     else
-      render json: user.errors.full_messages, status: :unprocessable_entity
+      render json: @user.errors.full_messages, status: :unprocessable_entity
     end
   end
 
   def destroy
-    user = User.find(params[:id])
-    user.destroy
-    render json: user
+    @user = User.find(params[:id])
+    @user.destroy
+    render json: @user
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:username)
   end
 end
